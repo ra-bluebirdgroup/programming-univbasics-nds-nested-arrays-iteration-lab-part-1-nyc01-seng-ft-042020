44016e6ab07b335762f9556b0daafd67cf8bec15
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-04-01 02:55:40 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -8,7 +8,7 @@ def find_even_values(src)
     while row_index < src.count do
       element_index = 0
       while element_index < src[row_index].count do
-        if src[row_index][element_index]%2 = 0
+        if src[row_index][element_index]%2 == 0
           puts src[row_index][element_index]
         # inside this loop, src[row_index][element_index] will access the current element
         element_index += 1
